274353d85e76984568ce609b240d019716f97456
wichakarn011 <wichakarn011@gmail.com>
2017-10-27 17:25:10 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1, maximum-scale=1">
-  <title>Web OCR Credit Detection</title>
-  <link rel="stylesheet" href="style-bootstrap.min.css" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1, maximum-scale=1">
+	<title>Web OCR Credit Detection</title>
+	<link rel="stylesheet" href="style-bootstrap.min.css" />
 </head>
 <body>
-  <strong>Index</strong>
-  <script src="javascript-jquery-3.2.1.min.js"></script>
-  <script src="javascript-popper.js"></script>
-  <script src="javascript-bootstrap.min.js"></script>
+	<nav class="navbar navbar-light bg-light">
+		<a class="navbar-brand" href="#">
+			<img src="image-camera.png" width="30" height="30" class="d-inline-block align-top" alt="">
+			OCR Credit
+		</a>
+	</nav>
+	<div class="container">
+		<br>
+		<div class="col-sm-12 col-xs-12 text-center">
+			<h1>Web Credit OCR</h1>
+		</div>
+		<br> <br>
+		<div class="col-sm-12 col-xs-12 text-center">
+			<h4>User Camera</h4>
+		</div>
+	</div>
+	<script src="javascript-jquery-3.2.1.min.js"></script>
+	<script src="javascript-popper.js"></script>
+	<script src="javascript-bootstrap.min.js"></script>
 </body>
 </html>
