f532b66e24b4037425061d6ca3b92c472f9c7e18
wichakarn011 <wichakarn011@gmail.com>
2017-10-27 17:53:39 +0700

    Add files via upload

---
+++ fragment-menu.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-dark bg-dark" >
+	<a class="navbar-brand" href="#">
+		<img src="image-camera.png" width="30" height="30" class="d-inline-block align-top" alt="">
+		OCR Credit
+	</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		<div class="navbar-nav">
+			<a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
+			<a class="nav-item nav-link disabled" href="#">Browser Support</a>
+			<a class="nav-item nav-link" href="#">Special Thank</a>
+		</div>
+	</div>
+</nav>
