14fbe24030278012c02665cf54c25307c126a264
wichakarn011 <wichakarn011@gmail.com>
2017-10-27 17:20:32 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1, maximum-scale=1">
   <title>Web OCR Credit Detection</title>
+  <link rel="stylesheet" href="style-bootstrap.min.css" />
 </head>
 <body>
   <strong>Index</strong>
   <script src="javascript-jquery-3.2.1.min.js"></script>
   <script src="javascript-popper.js"></script>
+  <script src="javascript-bootstrap.min.js"></script>
 </body>
 </html>
